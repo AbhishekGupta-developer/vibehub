07b76383096b3a11f3e9abb82188610967ff096d
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-02-05 19:34:52 +0530

    Add Get user, get all user, update uper and remove user methods

--- src/main/java/com/myorganisation/vibehub/repository/UserRepository.java
+++ src/main/java/com/myorganisation/vibehub/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.myorganisation.vibehub.model.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -20,5 +22,25 @@ public class UserRepository {
         users.put(id, user);
 
         return users.get(id);
+    }
+
+    public User getUser(Long id) {
+        return users.get(id);
+    }
+
+    public List<User> getAllUsers() {
+        return new LinkedList<>(users.values());
+    }
+
+    public User updateUser(User user) {
+        if(user.getId() != null) {
+            users.put(user.getId(), user);
+        }
+
+        return users.get(user.getId());
+    }
+
+    public void removeUser(Long id) {
+        users.remove(id);
     }
 }
