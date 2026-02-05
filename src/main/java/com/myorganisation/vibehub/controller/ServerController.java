2906ac6095b1f5195bc97c6acf8b421a8aa78fae
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-02-05 19:33:10 +0530

    Refactor code: Replace String with DTO

--- src/main/java/com/myorganisation/vibehub/controller/ServerController.java
+++ src/main/java/com/myorganisation/vibehub/controller/ServerController.java
@@ -1,5 +1,6 @@
 package com.myorganisation.vibehub.controller;
 
+import com.myorganisation.vibehub.dto.response.ServerResponseDto;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class ServerController {
     @GetMapping
-    public ResponseEntity<String> serverStatus() {
-        return new ResponseEntity<>("VibeHub is live", HttpStatusCode.valueOf(200));
+    public ResponseEntity<ServerResponseDto> serverStatus() {
+        return new ResponseEntity<>(new ServerResponseDto(), HttpStatusCode.valueOf(200));
     }
 }
