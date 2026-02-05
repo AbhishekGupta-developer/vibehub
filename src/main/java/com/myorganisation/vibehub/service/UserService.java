70052070e4d7ffc7fecbc0d868f84343306855c0
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-02-05 19:35:41 +0530

    Add Get user, get all user, update uper and remove user methods in User service

--- src/main/java/com/myorganisation/vibehub/service/UserService.java
+++ src/main/java/com/myorganisation/vibehub/service/UserService.java
@@ -1,8 +1,15 @@
 package com.myorganisation.vibehub.service;
 
 import com.myorganisation.vibehub.dto.request.UserRequestDto;
+import com.myorganisation.vibehub.dto.response.GenericResponseDto;
 import com.myorganisation.vibehub.dto.response.UserResponseDto;
+
+import java.util.List;
 
 public interface UserService {
     UserResponseDto addUser(UserRequestDto userRequestDto);
+    UserResponseDto getUser(Long id);
+    List<UserResponseDto> getAllUsers();
+    UserResponseDto updateUser(Long id, UserRequestDto userRequestDto);
+    GenericResponseDto removeUser(Long id);
 }
