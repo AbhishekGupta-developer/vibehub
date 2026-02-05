26859764c3830f9be7d3b8110e8fff9a58d3da39
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-02-05 19:36:58 +0530

    Add APIs to get user, get all users, update user and remove user

--- src/main/java/com/myorganisation/vibehub/controller/UserController.java
+++ src/main/java/com/myorganisation/vibehub/controller/UserController.java
@@ -1,16 +1,15 @@
 package com.myorganisation.vibehub.controller;
 
 import com.myorganisation.vibehub.dto.request.UserRequestDto;
+import com.myorganisation.vibehub.dto.response.GenericResponseDto;
 import com.myorganisation.vibehub.dto.response.UserResponseDto;
 import com.myorganisation.vibehub.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -22,5 +21,25 @@ public class UserController {
     @PostMapping
     public ResponseEntity<UserResponseDto> registerUser(@RequestBody UserRequestDto userRequestDto) {
         return new ResponseEntity<>(userService.addUser(userRequestDto), HttpStatusCode.valueOf(201));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUser(id), HttpStatusCode.valueOf(200));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatusCode.valueOf(200));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> updateUser(@PathVariable Long id, @RequestBody UserRequestDto userRequestDto) {
+        return new ResponseEntity<>(userService.updateUser(id, userRequestDto), HttpStatusCode.valueOf(200));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<GenericResponseDto> removeUser(@RequestParam Long id) {
+        return new ResponseEntity<>(userService.removeUser(id), HttpStatusCode.valueOf(200));
     }
 }
