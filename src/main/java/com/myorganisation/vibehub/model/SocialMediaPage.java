b9a5a63cd24ab446e7d57f6c75e914e4a398ec1a
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-02-09 19:52:35 +0530

    Add SocialMediaPage model

---
+++ src/main/java/com/myorganisation/vibehub/model/SocialMediaPage.java
@@ -0,0 +1,14 @@
+package com.myorganisation.vibehub.model;
+
+import com.myorganisation.vibehub.enums.PageCategory;
+
+import java.util.Date;
+
+public class SocialMediaPage {
+    private Long id;
+    private String name;
+    private PageCategory category;
+    private String description;
+    private Date createdOn;
+    private String password;
+}
