4b13d05ad0c6398e4886cab07f38e72678a1583a
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-02-13 19:53:17 +0530

    Convert User class into JPA entity

--- src/main/java/com/myorganisation/vibehub/model/User.java
+++ src/main/java/com/myorganisation/vibehub/model/User.java
@@ -1,15 +1,23 @@
 package com.myorganisation.vibehub.model;
 
 import com.myorganisation.vibehub.enums.Gender;
+import jakarta.persistence.*;
 import lombok.Data;
 
+@Entity
+@Table(name = "users")
 @Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String userName;
     private String password;
     private String email;
     private String phone;
+
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 }
