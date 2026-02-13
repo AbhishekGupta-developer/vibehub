28037392d0d961112bb6b0daaf4e50e154cdd52a
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-02-13 19:53:57 +0530

    Replace dummy DB with JpaRepository

--- src/main/java/com/myorganisation/vibehub/repository/UserRepository.java
+++ src/main/java/com/myorganisation/vibehub/repository/UserRepository.java
@@ -1,46 +1,9 @@
 package com.myorganisation.vibehub.repository;
 
 import com.myorganisation.vibehub.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
 @Repository
-public class UserRepository {
-    // Dummy Database
-    private Map<Long, User> users = new HashMap<>();
-
-    private static Long currentId = 0L;
-
-    public User addUser(User user) {
-        Long id = ++currentId;
-        user.setId(id);
-
-        users.put(id, user);
-
-        return users.get(id);
-    }
-
-    public User getUser(Long id) {
-        return users.get(id);
-    }
-
-    public List<User> getAllUsers() {
-        return new LinkedList<>(users.values());
-    }
-
-    public User updateUser(User user) {
-        if(user.getId() != null) {
-            users.put(user.getId(), user);
-        }
-
-        return users.get(user.getId());
-    }
-
-    public void removeUser(Long id) {
-        users.remove(id);
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
 }
