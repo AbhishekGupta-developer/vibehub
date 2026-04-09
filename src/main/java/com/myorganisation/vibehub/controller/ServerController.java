970d0e40c62e1decf47e623116a3d5364ba2952b
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-04-09 20:40:26 +0530

    Add POST type API in Server controller to test security

--- src/main/java/com/myorganisation/vibehub/controller/ServerController.java
+++ src/main/java/com/myorganisation/vibehub/controller/ServerController.java
@@ -4,6 +4,7 @@ import com.myorganisation.vibehub.dto.response.ServerResponseDto;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class ServerController {
 
-    @GetMapping("/test")
-    public String testApi() {
-        return "TEST API";
-    }
-
     @GetMapping
     public ResponseEntity<ServerResponseDto> serverStatus() {
         return new ResponseEntity<>(new ServerResponseDto(), HttpStatusCode.valueOf(200));
+    }
+
+    @GetMapping("/test")
+    public String testApi() {
+        return "TEST API - GET";
+    }
+
+    @PostMapping
+    public String testApiPost() {
+        return "TEST API - POST";
     }
 }
